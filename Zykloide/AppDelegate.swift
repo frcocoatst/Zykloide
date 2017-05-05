4077d920bb9921fa43e404a6b8395bdf3dd942e3
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-05-05 11:38:33 +0200

    Initial Commit

---
+++ Zykloide/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Zykloide
+//
+//  Created by Friedrich HAEUPL on 05.05.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Zykloide/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Zykloide
+//
+//  Created by Friedrich HAEUPL on 05.05.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
